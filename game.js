fc224afe23ee2b852e79d3c7b74cee1a86b24e1d
mehak <mehak@vt.edu>
2020-05-27 03:02:52 -0400

    Updates for CSS

--- game.js
+++ game.js
@@ -1,69 +1,130 @@
-cons cards = document.querySelectorAll('.memory-card');
+// const cards = document.querySelectorAll('.memory-card');
 
-let hasFlipped = false;
-let board = false;
+// let hasFlipped = false;
+// let board = false;
+// let firstCard, secondCard;
+
+// function flipCard()
+// {
+// 	if(board)
+// 		return;
+// 	if(this === firstCard)
+// 		return;
+
+// 	this.classList.add('flip');
+
+// 	if(!hasFlipped)
+// 	{
+// 		hasFlipped = true;
+// 		firstCard  = this;
+
+// 		return;
+// 	}
+
+// 	secondCard = this;
+// 	checkForMatch();
+// }
+
+// function checkForMatch()
+// {
+// 	let match = firstCard.dataset.framework === secondCard.dataset.framework;
+// 	match ? disableCards() : unflipCards();
+// }
+
+// function disableCards()
+// {
+// 	firstCard.removeEventListener('click', flipCard);
+// 	secondCard.removeEventListener('click', flipCard);
+
+// 	resetBoard();
+// }
+
+// function unflipCards()
+// {
+// 	board = true;
+
+// 	setTimeout(() => {
+// 		firstCard.classList.remove('flip');
+// 		secondCard.classList.remove('flip');
+
+// 		resetBoard();
+// 		}
+// 		1500);
+	
+// }
+
+// function resetBoard() {
+// 	[flipCard, board] = [false, false];
+// 	[firstCard, secondCard] = [null, null];
+// }
+
+// (function shuffle()
+// {
+// 	cards.forEach(card => {
+// 		let randomPosition = Math.floor(Math.random() * 12);
+// 		card.style.order = randomPosition;
+// 	});
+// })();
+
+// cards.forEach(card => card.addEventListener('click', flipCard));
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
-function flipCard()
-{
-	if(board)
-		return;
-	if(this === firstCard)
-		return;
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
 
-	this.classList.add('flip');
+  this.classList.add('flip');
 
-	if(!hasFlipped)
-	{
-		hasFlipped = true;
-		firstCard  = this;
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
 
-		return;
-	}
+    return;
+  }
 
-	secondCard = this;
-	checkForMatch();
+  secondCard = this;
+  checkForMatch();
 }
 
-function checkForMatch()
-{
-	let match = firstCard.dataset.framework === secondCard.dataset.framework;
-	match ? disableCards() : unflipCards();
+function checkForMatch() {
+  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+  isMatch ? disableCards() : unflipCards();
 }
 
-function disableCards()
-{
-	firstCard.removeEventListener('click', flipCard);
-	secondCard.removeEventListener('click', flipCard);
+function disableCards() {
+  firstCard.removeEventListener('click', flipCard);
+  secondCard.removeEventListener('click', flipCard);
 
-	resetBoard();
+  resetBoard();
 }
 
-function unflipCards()
-{
-	board = true;
+function unflipCards() {
+  lockBoard = true;
 
-	setTimeout(() => {
-		firstCard.classList.remove('flip');
-		secondCard.classList.remove('flip');
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
 
-		resetBoard();
-		}
-		1500);
-	
+    resetBoard();
+  }, 1500);
 }
 
 function resetBoard() {
-	[flipCard, board] = [false, false];
-	[firstCard, secondCard] = [null, null];
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
 
-(function shuffle()
-{
-	cards.forEach(card => {
-		let randomPosition = Math.floor(Math.random() * 12);
-		card.style.order = randomPosition;
-	});
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
 })();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
